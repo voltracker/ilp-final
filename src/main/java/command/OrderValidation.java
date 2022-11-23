3355f0714ea0a2b05ab20ef7c55d811b7090467b
Matthew Davidson <matthewd0224@gmail.com>
2022-11-23 03:32:30 +0000

    started implementation of order validation methods

--- src/main/java/command/OrderValidation.java
+++ src/main/java/command/OrderValidation.java
@@ -1,14 +1,60 @@
 package command;
 
+import logging.Logger;
 import model.Delivery;
 import model.Order;
+import model.OrderOutcome;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class OrderValidation {
 
-    public static List<Delivery> process(List<Order>){
+    public static List<Delivery> process(List<Order> orders){
         return null;
     }
-    
+
+    private static OrderOutcome validate(Order order){
+        return null;
+    }
+
+    private boolean validCardNumber(String cardNumber){
+        if (cardNumber.length() < 16){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean validExpiryDate(String expiryDate){
+        Logger log = Logger.getInstance();
+        try {
+            LocalDate expiry = LocalDate.parse(expiryDate);
+            // true if expiry date in the future, false if expiry date has passed
+            return !expiry.isBefore(LocalDate.now());
+        } catch (DateTimeParseException e) {
+            log.logAction("OrderValidation.validExpiryDate(expiryDate)", LogStatus.VALID_EXPIRY_DATE_PARSE_EXCEPTION);
+            return false;
+        }
+    }
+
+    private boolean validCvv(String cvv){
+        // matches if there are either 3 or 4 digits in string ONLY
+        String regex = "^\\d{3,4}$";
+        Pattern pattern = Pattern.compile(regex);
+        // returns true if cvv is valid, false otherwise
+        return pattern.matcher(cvv).find();
+    }
+
+    // TODO: implement luhn algorithm
+    private boolean luhnCheck(String cardNumber){
+        return false;
+    }
+
+    private enum LogStatus{
+        VALID_EXPIRY_DATE_PARSE_EXCEPTION
+    }
+
 }
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -8,13 +8,12 @@ import java.util.List;
 public class test {
     public static void main(String[] args){
         RestClient cli = new RestClient("https://ilp-rest.azurewebsites.net");
-        List<Order> orders = cli.getOrders("2022-01-10");
+        List<Order> orders = cli.getOrders("2023-01-10");
         for (Order order : orders){
-            System.out.println(order.orderNo());
-            for (String item : order.orderItems()){
-                System.out.print("--" + item + "--");
-            }
-            System.out.print("\n");
+            System.out.println(order.creditCardNumber());
+            System.out.println(order.creditCardExpiry());
+            System.out.println(order.cvv());
+            System.out.println("------------------");
         }
         Logger log = Logger.getInstance();
         for (LogItem item : log.getLog()){
