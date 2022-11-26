775b2756f4b96abc36065010b24d2369737ff81d
Matthew Davidson <matthewd0224@gmail.com>
2022-11-26 17:47:49 +0000

    changed pom and test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -12,6 +12,11 @@ public class test {
         RestClient cli = new RestClient("https://ilp-rest.azurewebsites.net");
         List<Polygon> nfz = cli.getNoFlyZones();
         List<Restaurant> restaurants = cli.getRestaurants();
+        List<Order> orders = cli.getOrders("2023-01-02");
+        List<Delivery> deliveries = OrderValidation.process(orders, restaurants);
+        for (Delivery delivery : deliveries) {
+            System.out.println(delivery.outcome());
+        }
         VisibilityGraph graph = new VisibilityGraph(nfz, restaurants);
         graph.buildGraph();
         System.out.println(graph.getGraph());
