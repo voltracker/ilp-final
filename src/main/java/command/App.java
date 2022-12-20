725d4c35e1ab69163eef52fdb4d09470d004ccb5
Matthew Davidson <matthewd0224@gmail.com>
2022-12-20 01:58:55 +0000

    added comments to main class

--- src/main/java/command/App.java
+++ src/main/java/command/App.java
@@ -9,17 +9,27 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
+/**
+ * Main entry point
+ */
 public class App {
+    /**
+     * Entry point into the program
+     * @param args command line arguments
+     */
     public static void main(String[] args){
+        // check that there are the correct number of arguments
         if (args.length != 3){
             System.err.println("Incorrect number of arguments, expected 3 got " + args.length);
         } else {
+            // set the url and date
             var date = args[0];
             var url = args[1];
-
+            // validate the date and url
             try {
                 LocalDate parsed = LocalDate.parse(date);
                 if (isValidURL(url)){
+                    // construct the deliverorders object, and start execution
                     DeliverOrders main = new DeliverOrders(date, url);
                     main.go();
                 }
